bed45f9da631d2950c7ff4e490ed822620ad63e1
Gongdolgun <159753min@gmail.com>
2023-12-27 19:11:17 +0900

    231217 이민학
    메인 프로젝트 Push
    
    [개인 테스트시]
    1. 게임 기본 맵 - Local Lobby
    2. 서버만 패키지
    3. 서버 exe 파일 바로가기 만들기
    4. 바로가기 파일 속성 클릭
    5. 대상 마지막에 -log 추가
    6. 바로가기 클릭해서 서버 열기
    7. 에디터에서 클라이언트 실행

---
+++ Source/Push/Push.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Push : ModuleRules
+{
+	public Push(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
