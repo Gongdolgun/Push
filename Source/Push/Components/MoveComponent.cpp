e2ef8cbf20d8c1b78171627df819c39068236582
Chujer <113624740+Chujer@users.noreply.github.com>
2023-12-28 14:19:20 +0900

    2023.12.28
    작업 이후 테스트

---
+++ Source/Push/Components/MoveComponent.cpp
@@ -0,0 +1,47 @@
+#include "MoveComponent.h"
+
+#include "GameFramework/Character.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UMoveComponent::UMoveComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void UMoveComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+void UMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
+void UMoveComponent::MoveForward(float InAxis)
+{
+	if (!IsValid(Owner.Get()))
+		return;
+
+	FVector direction = UKismetMathLibrary::GetForwardVector(FRotator(0, Owner->GetControlRotation().Yaw, 0));
+
+	Owner->AddMovementInput(direction, (SpeedX * SpeedPercent));
+
+}
+
+void UMoveComponent::MoveRight(float InAxis)
+{
+	if (!IsValid(Owner.Get()))
+		return;
+
+	FVector direction = UKismetMathLibrary::GetRightVector(FRotator(0, Owner->GetControlRotation().Yaw, 0));
+
+	Owner->AddMovementInput(direction, (SpeedY * SpeedPercent));
+
+}
+
---
+++ Source/Push/Components/MoveComponent.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MoveComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PUSH_API UMoveComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UMoveComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+		void MoveForward(float InAxis);
+	void MoveRight(float InAxis);
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Property")
+		float SpeedX;
+
+	UPROPERTY(EditAnywhere, Category = "Property")
+		float SpeedY;
+	UPROPERTY(EditAnywhere, Category = "Property")
+		float SpeedPercent;
+private:
+	TWeakObjectPtr<ACharacter> Owner;
+};
