a8585683bff5557545a2f9b9a250ec5b6736ecba
이현중 <113624740+Chujer@users.noreply.github.com>
2023-12-29 11:19:59 +0900

    Revert "2023.12.29 pullrequest"

--- Source/Push/Components/MoveComponent.cpp
+++ Source/Push/Components/MoveComponent.cpp
@@ -2,18 +2,18 @@
 
 #include "GameFramework/Character.h"
 #include "Kismet/KismetMathLibrary.h"
-#include "../Global.h"
 
 UMoveComponent::UMoveComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
-	Owner = Cast<APawn>(GetOwner());
+	
 }
 
 
 void UMoveComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	//Owner = GetOwner();
 }
 
 void UMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -22,29 +22,25 @@ void UMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 
 }
 
-void UMoveComponent::OnMoveForward(float InAxis)
+void UMoveComponent::MoveForward(float InAxis)
 {
 	if (!IsValid(Owner.Get()))
 		return;
+
 	FVector direction = UKismetMathLibrary::GetForwardVector(FRotator(0, Owner->GetControlRotation().Yaw, 0));
-	
-	Owner->AddMovementInput(direction, ((SpeedX * InAxis) * SpeedPercent));
+
+	Owner->AddMovementInput(direction, (SpeedX * SpeedPercent));
+
 }
 
-void UMoveComponent::OnMoveRight(float InAxis)
+void UMoveComponent::MoveRight(float InAxis)
 {
 	if (!IsValid(Owner.Get()))
-	{
-		FRotator rotate = Owner->GetActorRotation();
-		rotate.Yaw = rotate.Yaw + SpeedY * InAxis;
-		Owner->SetActorRotation(rotate);
-		
-
 		return;
-	}
+
 	FVector direction = UKismetMathLibrary::GetRightVector(FRotator(0, Owner->GetControlRotation().Yaw, 0));
 
-	Owner->AddMovementInput(direction, ((SpeedY * InAxis) * SpeedPercent));
+	Owner->AddMovementInput(direction, (SpeedY * SpeedPercent));
 
 }
 
