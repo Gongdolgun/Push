bce3ec70db5c0298763cd4b51c7dcfd514e64e7d
Chujer <113624740+Chujer@users.noreply.github.com>
2023-12-29 21:11:08 +0900

    버그 퇴치중

--- Source/Push/GameMode/PushGameMode.cpp
+++ Source/Push/GameMode/PushGameMode.cpp
@@ -30,33 +30,33 @@ void APushGameMode::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
-	if (MatchState == MatchState::WaitingToStart) // 대기
-	{
-		// 대기시간 - 현재시간 + 게임레벨맵에 들어간 시간
-		CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime;
-		if (CountdownTime <= 0.0f) // 대기시간이 끝나면 경기시작
-		{
-			StartMatch(); // 경기
-		}
-	}
-	else if (MatchState == MatchState::InProgress) // 경기
-	{
-		// 대기시간 - 현재시간 + 게임레벨맵에 들어간 시간 + 설정한 경기시간
-		CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime + MatchTime;
-		if (CountdownTime <= 0.0f)
-		{
-			SetMatchState(MatchState::Result); // 결과발표
-		}
-	}
-	else if (MatchState == MatchState::Result) // 결과발표
-	{
-		// 대기시간 - 현재 시간 + 게임레벨맵에 들어간 시간 + 설정한 경기시간 + 설정한 결과발표시간
-		CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime + MatchTime + ResultTime;
-		if (CountdownTime <= 0.0f) 
-		{
-			RestartGame(); // 경기 재시작. GameMode 내장 클래스에 정의된 함수 콜
-		}
-	}
+	//if (MatchState == MatchState::WaitingToStart) // 대기
+	//{
+	//	// 대기시간 - 현재시간 + 게임레벨맵에 들어간 시간
+	//	CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime;
+	//	if (CountdownTime <= 0.0f) // 대기시간이 끝나면 경기시작
+	//	{
+	//		StartMatch(); // 경기
+	//	}
+	//}
+	//else if (MatchState == MatchState::InProgress) // 경기
+	//{
+	//	// 대기시간 - 현재시간 + 게임레벨맵에 들어간 시간 + 설정한 경기시간
+	//	CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime + MatchTime;
+	//	if (CountdownTime <= 0.0f)
+	//	{
+	//		SetMatchState(MatchState::Result); // 결과발표
+	//	}
+	//}
+	//else if (MatchState == MatchState::Result) // 결과발표
+	//{
+	//	// 대기시간 - 현재 시간 + 게임레벨맵에 들어간 시간 + 설정한 경기시간 + 설정한 결과발표시간
+	//	CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime + MatchTime + ResultTime;
+	//	if (CountdownTime <= 0.0f) 
+	//	{
+	//		RestartGame(); // 경기 재시작. GameMode 내장 클래스에 정의된 함수 콜
+	//	}
+	//}
 
 }
 
