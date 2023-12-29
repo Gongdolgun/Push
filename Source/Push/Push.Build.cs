93c3794d696f3fd1b87522bde4b8ebef03cac751
InsuMoon2 <soo93689314@gmail.com>
2023-12-29 10:13:24 +0900

    231229_문인수
    스킬 만드는중

--- Source/Push/Push.Build.cs
+++ Source/Push/Push.Build.cs
@@ -7,6 +7,7 @@ public class Push : ModuleRules
 	public Push(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicIncludePaths.Add(ModuleDirectory);
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 	}
