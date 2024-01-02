d021bd2ae8f4ee6f177bccf56f3086446599e11a
Chujer <113624740+Chujer@users.noreply.github.com>
2024-01-02 11:22:28 +0900

    덮은후

--- Source/Push/GameMode/PushGameMode.cpp
+++ Source/Push/GameMode/PushGameMode.cpp
@@ -1,11 +1,10 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "PushGameMode.h"
-#include "Push/Character/PushCharacter.h"
+#include "Push/PushCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 APushGameMode::APushGameMode()
 {
+<<<<<<< HEAD
 	// set default pawn class to our Blueprinted character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP/Character/BP_PushCharacter"));
@@ -14,3 +13,13 @@ APushGameMode::APushGameMode()
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+=======
+
+}
+
+void APushGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+>>>>>>> parent of 34cbec8 (Merge branch 'Test' into HJ)
