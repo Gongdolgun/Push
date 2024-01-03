42a43c0b47769731af60b50abfc8d06dd64f2b33
grujam <63233571+grujam@users.noreply.github.com>
2024-01-03 10:33:19 +0900

    2024_01_02 서동주
    Character에 Hit Interface적용 데미지, 이펙트, 사운드 등 추가
    자기장에 피격 Widget 추가

--- Source/Push/Character/PushCharacter.h
+++ Source/Push/Character/PushCharacter.h
@@ -29,10 +29,17 @@ public:
 	UFUNCTION(BlueprintCallable)
 		void NumberPressed();
 
+public:
+	UPROPERTY(VisibleInstanceOnly)
+		class UWDG_EffectBase* widget;
+
 	class ASkill* SkillActor;
 
+	//2024_01_02 서동주 Hit Interface 적용
+	//다른 Actor에서 피격 시 Hit_Implementation을 Call해서 해주세요
 	virtual void Hit(const FHitData& InHitData) override {};
 	virtual void Hit_Implementation(const FHitData& InHitData) override;
+
 
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
@@ -42,7 +49,8 @@ private:
 		class UCameraComponent* FollowCamera;
 
 	UPROPERTY(VisibleAnywhere)
-		class UResourceComponent* ResorceComponent;
+		class UResourceComponent* ResourceComponent;
+
 	UPROPERTY(VisibleAnywhere)
 		class UMoveComponent* MoveComponent;
 
