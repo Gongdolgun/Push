04d3bf46916e77791acf5092af8b6db8ae9f1985
grujam <63233571+grujam@users.noreply.github.com>
2024-01-11 11:06:12 +0900

    Test Building

--- Source/Push/Skill/Area/AreaSkills/Skill_Meteor_A.cpp
+++ Source/Push/Skill/Area/AreaSkills/Skill_Meteor_A.cpp
@@ -12,15 +12,12 @@
 
 ASkill_Meteor_A::ASkill_Meteor_A()
 {
-<<<<<<< Updated upstream
 	Helpers::CreateComponent(this, &SphereComponent, "Collision", Root);
 	SphereComponent->SetSphereRadius(128.0f);
-=======
     bReplicates = true;
 
     Helpers::CreateComponent(this, &SphereComponent, "Collision", Root);
     SphereComponent->SetSphereRadius(128.0f);
->>>>>>> Stashed changes
 
 	Helpers::CreateActorComponent(this, &ProjectileComponent, "ProjectileComponent");
 	ProjectileComponent->InitialSpeed = 1000.0f;
@@ -30,12 +27,10 @@ ASkill_Meteor_A::ASkill_Meteor_A()
 
 void ASkill_Meteor_A::BeginPlay()
 {
-<<<<<<< Updated upstream
 	Super::BeginPlay();
 
 	Particle->SetVisibility(true);
 	Particle->Activate(true);
-=======
 
     Super::BeginPlay();
 
@@ -43,7 +38,6 @@ void ASkill_Meteor_A::BeginPlay()
     Particle->SetVisibility(true);
     Particle->Activate(true);
     ProjectileComponent->Activate(true);
->>>>>>> Stashed changes
 
 	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::ASkill_Meteor_A::OnComponentBeginOverlap);
 
@@ -67,7 +61,6 @@ void ASkill_Meteor_A::OnSkillClicked()
 {
 	Super::OnSkillClicked();
 
-<<<<<<< Updated upstream
 	//FString message = FString::Printf(TEXT("DecalTime : %f, InterpSpeed : %f"), DecalTime, InterpSpeed);
 
 	USkillComponent* skillComponent = Helpers::GetComponent<USkillComponent>(Owner);
@@ -79,7 +72,6 @@ void ASkill_Meteor_A::OnSkillClicked()
 	// skillComponent->SpawnLocation가 PointDecal의 위치이다.
 	// RelativeLocation은 메테오를 Spawn 시킬 위치 조절용
 	
-=======
     USkillComponent* skillComponent = Helpers::GetComponent<USkillComponent>(Owner);
 
     if (skillComponent == nullptr)
@@ -93,13 +85,11 @@ void ASkill_Meteor_A::OnSkillClicked()
     FVector direction = (skillComponent->SpawnLocation - Meteor_Location).GetSafeNormal();
     ProjectileComponent->Velocity = ProjectileComponent->InitialSpeed * (-direction);
     //Projectile_Server_Implementation(direction);
->>>>>>> Stashed changes
 
 	Meteor_Location = skillComponent->Meteor_StartLocation;
 	Meteor_Direction = Meteor_Location - skillComponent->SpawnLocation;
 	ProjectileComponent->Velocity = ProjectileComponent->InitialSpeed * (Meteor_Direction.GetSafeNormal());
 
-<<<<<<< Updated upstream
 	FString VectorAsString = Meteor_Location.ToString();
 	FString message = FString::Printf(TEXT("Meteor Location : %s "), *VectorAsString);
 	CLog::Print(message);
@@ -122,7 +112,6 @@ void ASkill_Meteor_A::OnSkillClicked()
 		DeferredDecal->SetDecalTime(fallingTime);
 		UGameplayStatics::FinishSpawningActor(DeferredDecal, decalTransform);
 	}
-=======
     FHitResult HitResult;
     TArray<AActor*> ignores;
     ignores.Add(Owner);
@@ -154,7 +143,6 @@ void ASkill_Meteor_A::OnSkillClicked()
     }
 
    
->>>>>>> Stashed changes
 
 	
 }
@@ -162,69 +150,43 @@ void ASkill_Meteor_A::OnSkillClicked()
 void ASkill_Meteor_A::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 	IDamageable* character = Cast<IDamageable>(OtherActor);
 
 	OnDestroy();
->>>>>>> Stashed changes
 
 	FVector start = DecalLocation;
 	FVector end = DecalLocation;
 
 	float radius = 150.0f;
-=======
 
-    IDamageable* character = Cast<IDamageable>(OtherActor);
-    USkillComponent* skillComponent = Helpers::GetComponent<USkillComponent>(Owner);
+	IDamageable* character = Cast<IDamageable>(OtherActor);
+	USkillComponent* skillComponent = Helpers::GetComponent<USkillComponent>(Owner);
 
-    if (skillComponent == nullptr)
-    {
-        return;
-    }
-
-    FVector CollisionLocation = skillComponent->SpawnLocation - skillComponent->curSkillData->RelativeLocation;
-
-    OnDestroy(CollisionLocation);
-
-    // 땅에 닿을 때, 캐릭터가 아니여야함
-    if (!Cast<APawn>(OtherActor))
-    {
-        FVector start = CollisionLocation;
-        FVector end = CollisionLocation;
-        float radius = 150.0f;
-
-        //CLog::Print(start);
-
-        // 데미지를 줄 Pawn만 추적
-        FHitResult Particle_HitResult;
-        TArray<TEnumAsByte<EObjectTypeQuery>> ObjectType;
-        ObjectType.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
->>>>>>> Stashed changes
-
-	// 데미지를 줄 Pawn만 추적
-	FHitResult Meteor_HitResult;
-	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectType;
-	ObjectType.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
-
-	TArray<AActor*> ignores;
-	ignores.Add(Owner);
-
-	UKismetSystemLibrary::SphereTraceSingleForObjects(Owner->GetWorld(), start, end, radius, ObjectType,
-		false, ignores, DrawDebug_Particle, Meteor_HitResult, true);
-
-	if (Meteor_HitResult.bBlockingHit)
+	if (skillComponent == nullptr)
 	{
-		// TODO : Hit Interface 데미지 처리
-		CLog::Print("Hit11");
-		APushCharacter* Character = Cast<APushCharacter>(Owner);
+		return;
+	}
 
-<<<<<<< Updated upstream
-		//Character->Hit_Implementation();
-=======
+	FVector CollisionLocation = skillComponent->SpawnLocation - skillComponent->curSkillData->RelativeLocation;
+
+	OnDestroy(CollisionLocation);
+
+	// 땅에 닿을 때, 캐릭터가 아니여야함
+	if (!Cast<APawn>(OtherActor))
+	{
+		FVector start = CollisionLocation;
+		FVector end = CollisionLocation;
+		float radius = 150.0f;
+
+		//CLog::Print(start);
+
 		// 데미지를 줄 Pawn만 추적
 		FHitResult Particle_HitResult;
+		TArray<TEnumAsByte<EObjectTypeQuery>> ObjectType;
+		ObjectType.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+
+		// 데미지를 줄 Pawn만 추적
+		FHitResult Meteor_HitResult;
 		TArray<TEnumAsByte<EObjectTypeQuery>> ObjectType;
 		ObjectType.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
 
@@ -232,30 +194,47 @@ void ASkill_Meteor_A::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedCom
 		ignores.Add(Owner);
 
 		UKismetSystemLibrary::SphereTraceSingleForObjects(Owner->GetWorld(), start, end, radius, ObjectType,
-			false, ignores, DrawDebug_Particle, Particle_HitResult, true);
+			false, ignores, DrawDebug_Particle, Meteor_HitResult, true);
 
-		if (Particle_HitResult.bBlockingHit)
+		if (Meteor_HitResult.bBlockingHit)
 		{
 			// TODO : Hit Interface 데미지 처리
-			if (character == nullptr)
-				return;
+			CLog::Print("Hit11");
+			APushCharacter* Character = Cast<APushCharacter>(Owner);
 
-			CLog::Print("Meteor Hit");
-			character->Hit(this, HitData);
+			//Character->Hit_Implementation();
+			// 데미지를 줄 Pawn만 추적
+			FHitResult Particle_HitResult;
+			TArray<TEnumAsByte<EObjectTypeQuery>> ObjectType;
+			ObjectType.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
 
+			TArray<AActor*> ignores;
+			ignores.Add(Owner);
+
+			UKismetSystemLibrary::SphereTraceSingleForObjects(Owner->GetWorld(), start, end, radius, ObjectType,
+				false, ignores, DrawDebug_Particle, Particle_HitResult, true);
+
+			if (Particle_HitResult.bBlockingHit)
+			{
+				// TODO : Hit Interface 데미지 처리
+				if (character == nullptr)
+					return;
+
+				CLog::Print("Meteor Hit");
+				character->Hit(this, HitData);
+
+			}
 		}
->>>>>>> Stashed changes
+
+
+		OnDestroy();
 	}
-
-
-	OnDestroy();
 }
 
 void ASkill_Meteor_A::OnDestroy(FVector InLocation)
 {
 	Super::OnDestroy();
 
-<<<<<<< Updated upstream
 	// 폭발 파티클 소환
 	if (Explosion)
 	{
@@ -263,25 +242,21 @@ void ASkill_Meteor_A::OnDestroy(FVector InLocation)
 		explosionTramsform.SetLocation(DecalLocation);
 		explosionTramsform.SetScale3D(ExplosionScale);
 		UGameplayStatics::SpawnEmitterAtLocation(Owner->GetWorld(), Explosion, explosionTramsform);
-=======
-    // 폭발 파티클 소환
-    if (Explosion)
-    {
-        FTransform explosionTramsform;
-        explosionTramsform.SetLocation(InLocation);
-        explosionTramsform.SetScale3D(ExplosionScale);
-        UGameplayStatics::SpawnEmitterAtLocation(Owner->GetWorld(), Explosion, explosionTramsform);
->>>>>>> Stashed changes
+		// 폭발 파티클 소환
+		if (Explosion)
+		{
+			FTransform explosionTramsform;
+			explosionTramsform.SetLocation(InLocation);
+			explosionTramsform.SetScale3D(ExplosionScale);
+			UGameplayStatics::SpawnEmitterAtLocation(Owner->GetWorld(), Explosion, explosionTramsform);
 
-		Particle->SetIsReplicated(true);
-		Particle->SetActive(false);
+			Particle->SetIsReplicated(true);
+			Particle->SetActive(false);
+		}
+
+
+
 	}
-
-	
-
-<<<<<<< Updated upstream
-}
-=======
 
 }
 
@@ -297,4 +272,3 @@ void ASkill_Meteor_A::Projectile_NMC_Implementation(FVector InVelocity)
     CLog::Print(ProjectileComponent->Velocity);
 }
 
->>>>>>> Stashed changes
