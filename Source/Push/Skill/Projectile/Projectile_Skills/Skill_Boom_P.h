5c58d3cae4b440f0787daea179f3856a0f8db2c0
Gongdolgun <159753min@gmail.com>
2024-01-25 21:18:21 +0900

    24.01.25
    skill_boom! 제작

---
+++ Source/Push/Skill/Projectile/Projectile_Skills/Skill_Boom_P.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Skill/Projectile/Projectile.h"
+#include "Skill_Boom_P.generated.h"
+
+UCLASS()
+class PUSH_API ASkill_Boom_P : public AProjectile
+{
+	GENERATED_BODY()
+
+public:
+	ASkill_Boom_P();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void Destroyed() override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Explosion Particle")
+		class UParticleSystem* Explosion;
+
+	UPROPERTY(EditAnywhere)
+		class USphereComponent* DetectTarget;
+
+	UPROPERTY(EditAnywhere)
+		float Speed = 5.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		class APushCharacter* Target;
+
+public:
+	virtual void FOnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+};
